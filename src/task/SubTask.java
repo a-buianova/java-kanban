f0b1b3cc11ee9a50ef19756256a75770c60bff9e
a-buianova <69196277+a-buianova@users.noreply.github.com>
2025-02-17 18:05:22 +0100

    Sprint 6 solution (#1)
    * Implemented Tests
    
    * Sprint 6: business logic and tests
    
    * fix(tests): fixed failing tests
    
    * Fix CheckStyle errors
    
    * Refactor: Removed duplicate removeEpic method
    
    * Fix: Return actual lists in getAllTasks() and getAllEpics()
    
    * test: add edge case tests for HistoryManager

--- src/task/SubTask.java
+++ src/task/SubTask.java
@@ -1,15 +1,37 @@
 package task;
 
-public class SubTask extends Task {
+public class SubTask extends Task implements Cloneable {
     private int epicId;
 
     public SubTask(int id, String title, String description, TaskStatus status, int epicId) {
         super(id, title, description, status);
+
+        if (epicId <= 0) {
+            throw new IllegalArgumentException("ID эпика должен быть положительным числом.");
+        }
+
         this.epicId = epicId;
     }
 
     public int getEpicId() {
         return epicId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        SubTask subTask = (SubTask) o;
+        return epicId == subTask.epicId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + epicId;
+        return result;
     }
 
     @Override
@@ -21,5 +43,12 @@ public class SubTask extends Task {
                 ", status=" + getStatus() +
                 ", epicId=" + epicId +
                 '}';
+    }
+
+
+
+    @Override
+    public SubTask clone() {
+        return (SubTask) super.clone();
     }
 }
